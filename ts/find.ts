4cbc1830b6939aaee86fb01a13ad17df68b5818b
e <550893685@qq.com>
2020-12-07 22:31:16 +0800

    ts-find

--- ts/find.ts
+++ ts/find.ts
@@ -1,29 +1,28 @@
-"use strict";
 /**
  * find [在指定数组中查找包含指定键值的子项并返回该子项]
  * @param  {[Array]} arrayData [待遍历查找数据]
  * @param  {[Object]} keyItem [指定键值对象]
  * @return {[Object]} findItem [匹配对象]
  */
-Object.defineProperty(exports, "__esModule", { value: true });
-let find = (arrayData, keyItem, getIndex) => {
-    if (!(arrayData && keyItem)) {
+
+let find = (arrayData: Object[], keyItem: Object, getIndex: number): Object | null | undefined => {
+    if(!(arrayData && keyItem)){
         return;
     }
-    let findItem = null, findIndex = -1, matchCount, keyArray = [], canContinue = true;
-    for (let i in keyItem) {
-        if (keyItem.hasOwnProperty(i)) {
+    let findItem:null | Object = null, findIndex: number = -1, matchCount:number, keyArray: string[] = [], canContinue: boolean = true;
+    for(let i in keyItem){
+        if(keyItem.hasOwnProperty(i)){
             keyArray.push(i);
         }
     }
-    arrayData.forEach((item, index) => {
+    arrayData.forEach((item: Object,index: number)=>{
         matchCount = 0;
-        keyArray.forEach(key => {
-            if (item[key] === keyItem[key]) {
+        keyArray.forEach(key=>{
+            if(item[key] === keyItem[key]){
                 matchCount++;
             }
         });
-        if (matchCount === keyArray.length && canContinue) {
+        if(matchCount === keyArray.length && canContinue){
             findItem = item;
             findIndex = index;
             canContinue = false;
@@ -31,4 +30,5 @@ let find = (arrayData, keyItem, getIndex) => {
     });
     return getIndex ? findIndex : findItem;
 };
-exports.default = find;
+
+export default find;
